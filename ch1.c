7bf4dfc0ae82b9d90debdda90a3b6c8141b727e7
June <hjs1597@gmail.com>
2023-08-02 00:15:46 +0900

    ch1 review

---
+++ ch1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+#define	Lower	0
+#define Upper	300
+#define Step	20
+
+//int main() {
+//	int fahr;
+//
+//	for (fahr = Lower; fahr < Upper; fahr = fahr + Step) {
+//		printf("%3d %6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32));
+//	}
+//}
+
+int main() {
+	/*int c;
+
+	c = getchar();
+	while (c != EOF) {
+		putchar(c);
+		c = getchar();
+	}*/
+
+	int c;
+
+	while ((c = getchar()) != EOF) {
+		putchar(c);
+		printf("%d", EOF);
+	}
+
+}
